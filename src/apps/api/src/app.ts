dcb505584c2db18f86e79384f7f042678430693a
Ajims Ismail <ajimsismail123@gmail.com>
2026-02-12 20:17:07 +0530

    Added cors orgin

--- src/apps/api/src/app.ts
+++ src/apps/api/src/app.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'https://olx-rho-eight.vercel.app/',
+        'https://olx-rho-eight.vercel.app',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
